948958ab700705d61fdc319c79f5714ff71b3067
ngochai1242003 <dangngochai120403@gmail.com>
2025-04-16 16:48:48 +0700

    feat: implement promobanner component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,14 @@
+import PromoBanners from "./_components/section-promo-banners";
+import TestimonialsCarousel from "./_components/section-testimonial/testimonial";
+
 const Home = () => {
   return (
-    <div className="text-3xl flex justify-center items-center">Home page</div>
+    <div className="bg-[#FAF9F8]">
+      <section className="mx-auto px-[20px] lg:px-[255px]">
+        <PromoBanners />
+      </section>
+      <TestimonialsCarousel />
+    </div>
   );
 };
 
